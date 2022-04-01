6fbf9762ad5b172519e83443a9fd0f3822cb0f67
Moabe <moabeluis2018@gmail.com>
2022-03-31 22:03:44 -0300

    testando o useState

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
-import React, { useState,useEffect } from "react";
+import React, { useState, useEffect } from "react";
 
 export default function App() {
+
+  const [repositories, setRepositories] = useState([
+    { id: 1, name: 'repo-1' },
+    { id: 2, name: 'repo-2' },
+    { id: 4, name: 'repo-3' }
+  ])
+
+  function handleAddRepository(){
+    setRepositories([
+      ...repositories, 
+      {id: Math.random(), name: " Novo repo"}
+  ])
+  }
+
+
   return(
-    <h1>Hello World</h1>
+    <>
+      <ul>
+        {repositories.map( repo => (
+          <li key={repo.id}>{repo.name}</li>
+        ))}
+      </ul>
+      <button onClick={handleAddRepository}>
+        Adicionar repositório
+      </button>
+    </>  
   )
 }
