5527ea933b28c0a81730e295c4699a742dc211be
Moabe <moabeluis2018@gmail.com>
2022-03-31 22:46:05 -0300

    Testando EventListenner

--- src/App.js
+++ src/App.js
@@ -2,38 +2,22 @@ import React, { useState, useEffect } from "react";
 
 export default function App() {
 
-  const [repositories, setRepositories] = useState([])
+  const [location, setLocation] = useState({})
 
-  useEffect(async() => {
-    const response = await fetch('https://api.github.com/users/moabedebug/repos')
-    const data = await response.json()
-
-    setRepositories(data)
+  useEffect(() => {
+    navigator.geolocation.watchPosition(handlePositionReceived)
   },[])
 
-    useEffect(() => {
-      const filtered = repositories.filter( repo => repo.favorite)
+  function handlePositionReceived({ coords }) {
+    const { latitude, longitude } = coords
 
-      document.title = `Você tem ${filtered.length} favoritos`
-    }, [repositories])
-
-   function handleFavorite(id) {
-     const newRepositories = repositories.map(repo => {
-       return repo.id === id ? { ...repo, favorite: !repo.favorite} : repo
-     })
-
-     setRepositories(newRepositories)
-   }
+    setLocation({ latitude, longitude })
+  }
 
   return(
-    <ul>
-      {repositories.map( repo => (
-        <li key={repo.id}>
-          {repo.name} 
-          {repo.favorite && <span>(Favorito)</span>}
-          <button onClick={() => handleFavorite(repo.id)}>Favoritar</button>
-        </li>
-      ))}
-    </ul>
+    <>
+      Latitude: {location.latitude} <br/>
+      Longitude: {location.longitude}
+    </>
   )
 }
