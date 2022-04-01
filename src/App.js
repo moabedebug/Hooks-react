399c64131782ef7e151b4995df5cb833baf1a6ba
Moabe <moabeluis2018@gmail.com>
2022-03-31 22:36:42 -0300

    Testando useEffect

--- src/App.js
+++ src/App.js
@@ -2,30 +2,38 @@ import React, { useState, useEffect } from "react";
 
 export default function App() {
 
-  const [repositories, setRepositories] = useState([
-    { id: 1, name: 'repo-1' },
-    { id: 2, name: 'repo-2' },
-    { id: 4, name: 'repo-3' }
-  ])
+  const [repositories, setRepositories] = useState([])
 
-  function handleAddRepository(){
-    setRepositories([
-      ...repositories, 
-      {id: Math.random(), name: " Novo repo"}
-  ])
-  }
+  useEffect(async() => {
+    const response = await fetch('https://api.github.com/users/moabedebug/repos')
+    const data = await response.json()
 
+    setRepositories(data)
+  },[])
+
+    useEffect(() => {
+      const filtered = repositories.filter( repo => repo.favorite)
+
+      document.title = `Você tem ${filtered.length} favoritos`
+    }, [repositories])
+
+   function handleFavorite(id) {
+     const newRepositories = repositories.map(repo => {
+       return repo.id === id ? { ...repo, favorite: !repo.favorite} : repo
+     })
+
+     setRepositories(newRepositories)
+   }
 
   return(
-    <>
-      <ul>
-        {repositories.map( repo => (
-          <li key={repo.id}>{repo.name}</li>
-        ))}
-      </ul>
-      <button onClick={handleAddRepository}>
-        Adicionar repositório
-      </button>
-    </>  
+    <ul>
+      {repositories.map( repo => (
+        <li key={repo.id}>
+          {repo.name} 
+          {repo.favorite && <span>(Favorito)</span>}
+          <button onClick={() => handleFavorite(repo.id)}>Favoritar</button>
+        </li>
+      ))}
+    </ul>
   )
 }
